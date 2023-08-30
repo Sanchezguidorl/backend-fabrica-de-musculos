fc3e2f0adc8c8e278ae9c18f279ea607a0b3974e
Sanchezguidorl <sanchezguidorl@gmail.com>
2023-08-30 12:31:05 -0300

    Se modifica cors

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-app.use(cors({
-  origin: '*',
-  methods:'*',
-  allowedHeaders:'*'
-}));
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
