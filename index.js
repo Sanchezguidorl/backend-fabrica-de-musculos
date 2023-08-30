d21150a2a3633787379bce13e8e504f290d58d0a
Sanchezguidorl <sanchezguidorl@gmail.com>
2023-08-30 13:53:09 -0300

    Se agrega header content type a index

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 const imagenesRouter = require("./imagenes.routes.js");
-const morgan = require("morgan");
 const { connectDB } = require("./database.js");
 const buttonsRouter = require("./buttons.routes.js");
 const userRouter = require("./user.routes.js");
@@ -9,13 +8,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.use((req, res, next) => {
-  
+
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type'); // Agregado Content-Type
   res.setHeader('Access-Control-Expose-Headers', 'Authorization');
   next();
 });
-app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(userRouter);
