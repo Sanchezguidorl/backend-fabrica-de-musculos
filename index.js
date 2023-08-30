f8a97b9eeeff21b28c932619a1ea696ca72a312b
Sanchezguidorl <sanchezguidorl@gmail.com>
2023-08-30 11:58:34 -0300

    Se modifica cors

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-app.use(cors({
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
